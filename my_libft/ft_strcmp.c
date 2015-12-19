54508cc634f1029cff68d986469990c2d4b9e7da
jmontija <julien.montijano@gmail.com>
2015-12-19 14:03:36 +0100

    part_1

--- my_libft/ft_strcmp.c
+++ my_libft/ft_strcmp.c
@@ -10,22 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_strcmp(char *s1, char *s2)
+#include "libft.h"
+
+int		ft_strcmp(const char *s1, const char *s2)
 {
 	int i;
 
 	i = 0;
-	while (s1[i] == s2[i])
-	{
-		if (s1[i] == '\0' && s2[i] == '\0')
-			return (0);
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 		i++;
-	}
-	if (s1[i] == '\0' && s1[i] != s2[i])
-		return (0 - s2[i]);
-	else if (s2[i] == '\0' && s1[i] != s2[i])
-		return (s1[i]);
-	else
-		return (s1[i] - s2[i]);
-	return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
--- my_libft/ft_strncmp.c
+++ my_libft/ft_strncmp.c
@@ -10,23 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_strncmp(char *s1, char *s2, unsigned int n)
+#include "libft.h"
+
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned int i;
+	size_t i;
 
 	i = 0;
-	while (s1[i] == s2[i] && i < n -1)
-	{
-		if (s1[i] == '\0' && s2[i] == '\0')
-			return (0);
-		else
-			i++;
-	}
-	if (s1[i] == '\0' && s1[i] != s2[i])
-		return (0 - s2[i]);
-	else if (s2[i] == '\0' && s1[i] != s2[i])
-		return (s1[i]);
-	else
-		return (s1[i] - s2[i]);
-	return (0);
+	if (n == 0)
+		return (0);
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i < n - 1)
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
