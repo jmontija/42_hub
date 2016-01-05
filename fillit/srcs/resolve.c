d4fcdd415d77966aa05e3d3d0cefb114105a5aa7
jmontija <julien.montijano@gmail.com>
2016-01-05 12:56:49 +0100

    fillit

--- fillit/srcs/resolve.c
+++ fillit/srcs/resolve.c
@@ -6,23 +6,39 @@
 /*   By: julio <julio@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/04 01:07:51 by julio             #+#    #+#             */
-/*   Updated: 2016/01/05 12:44:26 by julio            ###   ########.fr       */
+/*   Updated: 2016/01/05 12:56:21 by julio            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-/*void	ft_algo(group *grp, tetrim *curr, int x, int y)
+/*int count_dot(char *line)
+{
+	int i;
+
+	i = 0;
+	while (line[i] == '.' && line[i] != '\0')
+		i++;
+	return (i);
+}
+
+void	ft_algo(group *grp, tetrim *curr, int x, int y)
 {
 	char **map;
 	char **shape;
+	int i;
+	int j;
+	int dot;
 
 	map = grp->map;
 	shape = curr->shape;
+	i = 0;
+	j = 0;
+	dot = 0
 	if (curr != NULL)
 	{
-		if (map[x][y] == '.' && curr)
-
+		if (map[y][x] == '.')
+			dot = count_dot(&map[y]);
 	}
 }
 
