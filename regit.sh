314f2b974e2d107e42455d1b48b7a6145db62713
jmontija <julien.montijano@gmail.com>
2016-01-18 18:32:47 +0100

    update

--- regit.sh
+++ regit.sh
@@ -4,5 +4,4 @@ git config user.email "julien.montijano@gmail.com"
 git config push.default "simple"
 git add -A
 git commit -m "update"
-git remote add origin https://github.com/jmontija/fillit.git
 git push -u origin master
